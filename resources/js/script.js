70347d5ac1c70ce320dd15498f6ace346cada761
pd7xx <p.digitalx@gmail.com>
2018-11-04 22:27:17 +0000

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function() {
+
+    /* for sticky navigation A Plugin see notes*/
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+    
+    /*LEC-65 Scroll on buttons*/
+    $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+    
+    /*LEC-65-Smooth scrolling - jquery code - Taken from https://css-tricks.com/snippets/jquery/smooth-scrolling*/
+    
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+    /*Animations on scroll*/
+    /*L66 section get food fast*/
+    $('.js--wp-1').waypoint(function(direction) {
+        $9('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    /*L66 How it works*/
+    $('.js--wp-2').waypoint(function(direction) {
+        $9'.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    /*L66 currently in these cities*/
+    $('.js--wp-3').waypoint(function(direction) {
+        $9'.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    /*L66 start eating healthy today - price plan*/
+    $('.js--wp-4').waypoint(function(direction) {
+        $9'.js--wp-4').addClass('animated pluse');
+    }, {
+        offset: '50%'
+    });
+    
+});
